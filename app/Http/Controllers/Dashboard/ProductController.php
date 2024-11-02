feb2868b6ea678ded0d2485691c8e314e3c43d81
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-03 03:10:54 +0700

    Update ProductController.php

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -138,7 +138,7 @@ class ProductController extends Controller
         $profit = 0.3;
         $packingPrice = 500;
         $total = $appFee + $profit;
-        $adjustment = $variant->price / (1 + $total) - $packingPrice;
+        $adjustment = ($variant->price - $packingPrice) / (1 + $total);
         $price = $variant->price - $adjustment - $packingPrice;
         dd(
             $variant->price,
