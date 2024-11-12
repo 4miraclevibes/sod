a3f991a7ac0d78a5d5ecf3fd23489530785abd7e
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-12 17:45:47 +0700

    Update footer.blade.php

--- resources/views/layouts/landing/footer.blade.php
+++ resources/views/layouts/landing/footer.blade.php
@@ -11,7 +11,7 @@
                 <a href="{{ route('cart') }}" class="text-decoration-none position-relative @auth {{ Auth::user()->role->name == 'driver' ? 'disabled' : '' }} @endauth {{ Route::is('cart*') ? 'active' : '' }}">
                     @auth
                         @if(Auth::user()->carts->count() > 0)
-                            <span class="position-absolute top-0 start-75 translate-middle badge rounded-pill bg-danger">
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success" style="font-size: 10px; margin-left: -2rem; margin-top: -4px">
                                 {{ Auth::user()->carts->count() }}
                             </span>
                         @endif
