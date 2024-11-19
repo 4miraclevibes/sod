81768a41c83698539780812d95e56d60cbe221a8
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-19 17:27:03 +0700

    c

--- resources/views/layouts/landing/main.blade.php
+++ resources/views/layouts/landing/main.blade.php
@@ -8,10 +8,10 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-mobile-web-app-title" content="Sayur On Delivery">
-    <link rel="apple-touch-icon" href="{{ asset('') }}">
-    <link rel="icon" href="{{ asset('logo.png') }}" type="image/x-icon">
+    <link rel="apple-touch-icon" href="{{ asset('logo-green.png') }}">
+    <link rel="icon" href="{{ asset('logo-green.png') }}" type="image/x-icon">
     <link rel="manifest" href="{{ asset('manifest.json') }}">
-    <link rel="service-worker" href="{{ asset('service-worker.js') }}">
+    <link rel="service-worker" href="{{ asset('sw.js') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
