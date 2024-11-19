9e67e6ec54b5f07fc322eb5850f58c9ea563bf44
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-19 17:39:57 +0700

    c

--- resources/views/layouts/landing/main.blade.php
+++ resources/views/layouts/landing/main.blade.php
@@ -8,8 +8,8 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-mobile-web-app-title" content="Balian">
-    <link rel="apple-touch-icon" href="{{ asset('logo-green.png') }}">
-    <link rel="icon" href="{{ asset('logo-green.png') }}" type="image/x-icon">
+    <link rel="apple-touch-icon" href="{{ asset('logo-green-mark.png') }}">
+    <link rel="icon" href="{{ asset('logo-green-mark.png') }}" type="image/x-icon">
     <link rel="manifest" href="{{ asset('manifest.json') }}">
     <link rel="service-worker" href="{{ asset('sw.js') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
