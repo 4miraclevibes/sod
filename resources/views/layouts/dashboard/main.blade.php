d2283a7e6c7beac1bc6a4ce9902b13a0896a40b1
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-26 15:17:50 +0700

    Update main.blade.php

--- resources/views/layouts/dashboard/main.blade.php
+++ resources/views/layouts/dashboard/main.blade.php
@@ -32,22 +32,22 @@
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css') }}" />
-    
+
     <!-- Core CSS -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
-    
+
     <!-- Vendors CSS -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-    
+
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css') }}" />
-    
+
     <!-- Page CSS -->
-    
+
     <!-- Helpers -->
     <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
-    
+
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
       <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
       <script src="{{ asset('assets/js/config.js') }}"></script>
@@ -158,7 +158,7 @@
 
           <!-- Content wrapper -->
           <div class="content-wrapper">
-           
+
             @yield('content')
 
             @include('layouts.dashboard.footer')
@@ -200,7 +200,7 @@
           });
       });
     </script>
-    @if(Route::is('dashboard.district*')) 
+    @if(Route::is('dashboard.district*'))
     <script>
       $(document).ready(function () {
           $('#example1').DataTable({
